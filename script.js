7e7bbae02930ea36424dd495ebae6954e9ab6d9c
damianludwiczak89 <damian_konin@tlen.pl>
2023-06-07 22:57:21 +0200

    js cleanup

--- script.js
+++ script.js
@@ -3,17 +3,9 @@ const about = document.querySelector('#introduction');
 const projects = document.querySelector('#projects');
 const certs = document.querySelector('#certs');
 
-const about_button = document.querySelector('#about_button');
-const projects_button = document.querySelector('#projects_button');
-const certs_button = document.querySelector('#certs_button');
-
 const hangman_desc = document.querySelector('#hangman_desc');
 const fit_desc = document.querySelector('#fit_desc');
 const truck_desc = document.querySelector('#truck_desc');
-
-const hangman_button = document.querySelector('#hangman_button');
-const fit_button = document.querySelector('#fit_button');
-const truck_button = document.querySelector('#truck_button');
 
 projects.style.display = "none";
 certs.style.display = "none";
@@ -22,40 +14,32 @@ truck_desc.style.display = "none";
 fit_desc.style.display = "none";
 
 
-about_button.onclick = function () {
-    about.style.display = "block";
-    projects.style.display = "none";
-    certs.style.display = "none";
-    
-};
 
-projects_button.onclick = function () {
-    hangman_desc.style.display = "none";
-    truck_desc.style.display = "none";
-    fit_desc.style.display = "none";
-    about.style.display = "none";
-    projects.style.display = "block";
-    certs.style.display = "none";
-};
+const sections = document.querySelectorAll('.section');
+const menu = document.querySelectorAll('#menu_button');
+const buttons = document.querySelectorAll('.project_button');
+const divs = document.querySelectorAll('.project_desc');
 
-certs_button.onclick = function () {
-    about.style.display = "none";
-    projects.style.display = "none";
-    certs.style.display = "block";
-};
 
-hangman_button.onclick = function () {
-    hangman_desc.style.display = "block";
-    truck_desc.style.display = "none";
-    fit_desc.style.display = "none";
-};
-fit_button.onclick = function () {
-    hangman_desc.style.display = "none";
-    truck_desc.style.display = "none";
-    fit_desc.style.display = "block";
-};
-truck_button.onclick = function () {
-    hangman_desc.style.display = "none";
-    truck_desc.style.display = "block";
-    fit_desc.style.display = "none";
-};
+menu.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      sections.forEach(function(section) {
+        section.style.display = "none";
+      });
+        sections[index].style.display = "block";
+        hangman_desc.style.display = "none";
+        truck_desc.style.display = "none";
+        fit_desc.style.display = "none";
+    });
+  });
+
+
+
+buttons.forEach(function(button, index) {
+    button.addEventListener('click', function() {
+      divs.forEach(function(div) {
+        div.style.display = "none";
+      });
+      divs[index].style.display = "block";
+    });
+  });
